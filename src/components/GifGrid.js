a50da2d17835364c577da758a01ce01d7a4ebb58
Luis Enrique Ocampo Gonzalez <blash3g@gmail.com>
2021-05-17 15:33:27 -0500

    Creado los custom hook El cual funciona por aparte para que puedas simplemente llamar la funcion de lo que necesites.

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,20 +1,15 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
+import { useFetchGift } from '../hooks/useFetchGift'
 import { GifGridItem } from './GifGridItem';
-import { getGift } from '../helpers/getGifs'
 
 export const GifGrid = ({category}) => {
 
-    const [images, setImages] = useState([]);
-
-    useEffect( () => {
-        getGift( category )
-            //img => setImages(img) recibe un json que se agregaran a la imagen
-            .then(setImages);
-    }, [ category ])
+    const  {data:images,loading} = useFetchGift(category);
 
     return (
         <>
             <h3>{category}</h3>
+            { loading && <p>Cargando..</p>}
             <div className="card-grid">
                 {images.map( (img) => (
                     <GifGridItem 
---
+++ src/hooks/useFetchGift.js
@@ -0,0 +1,23 @@
+import {useState, useEffect} from 'react'
+import { getGift } from '../helpers/getGifs'
+
+export const useFetchGift = (category)=> {
+    const [state, setState] = useState({
+        data: [],
+        loading: true
+    })
+
+    useEffect( () => {
+        getGift(category)
+            .then(imgs => {
+                setTimeout(()=>{
+                    setState({
+                        data: imgs,
+                        loading: false
+                    });
+                }, 3000);
+            })
+    }, [ category ])
+
+    return state;
+}
